2f67434b09ef6b12c70e66fece1c1c127990267c
Bruno <brunocoio@gmail.com>
2022-05-26 16:53:03 -0300

    criando read/edit no controller

--- controller/DefaultController.php
+++ controller/DefaultController.php
@@ -13,31 +13,51 @@ class DefaultController
   /**
    * read
    */
-  public function read($table, $where = null, $order = null, $limit = null)
+  public function read($table, $where = null, $order = null, $limit = null, $type = null)
   {
     $query = (new Connect($table))->select($where, $order, $limit)
       ->fetchAll(PDO::FETCH_CLASS, self::class);
-
-    print "<table class='table table-striped table-hover'>";
-    print "
-      <thead>
-        <tr>
-          <th scope='col'>ID</th>
-          <th scope='col'>Nome</th>
-          <th scope='col'>Email</th>
-          <th scope='col'>Password</th>
-          <th scope='col'>Ação</th>
-        </tr>
-      </thead>
-    ";
-    foreach ($query as $val) {
-      print "<tr>";
-      print "<td>{$val->id}</td><td>{$val->name}</td><td>{$val->email}</td><td>{$val->password}</td>";
-      print "<td><a href='' target='self' class='btn btn-outline-warning mx-2'><i class='fas fa-pencil-alt'></i></a>";
-      print "<a href='' target='self' class='btn btn-outline-danger mx-2'><i class='fas fa-trash'></i></a></td>";
-      print "</tr>";
+    switch ($type) {
+      case 'read':
+        foreach ($query as $val) {
+          print "
+          <div class'mb-3'>
+            <label for='name' class='form-label'>Nome</label>
+            <input type='text' value='{$val->name}' class='form-control' id='name' placeholder='Nome'>
+          </div>
+          <div class'mb-3'>
+            <label for='email' class='form-label'>E-mail</label>
+            <input type='email' value='{$val->email}' class='form-control' id='email' placeholder='Email'>
+          </div>
+          <div class'mb-3'>
+            <label for='password' class='form-label'>Senha</label>
+            <input type='text' value='{$val->password}' class='form-control' id='password' placeholder='Senha'>
+          </div>
+          ";
+        }
+        break;
+      default:
+        print "<table class='table table-striped table-hover'>";
+        print "
+        <thead>
+          <tr>
+            <th scope='col'>ID</th>
+            <th scope='col'>Nome</th>
+            <th scope='col'>Email</th>
+            <th scope='col'>Password</th>
+            <th scope='col'>Ação</th>
+          </tr>
+        </thead>
+        ";
+        foreach ($query as $val) {
+          print "<tr>";
+          print "<td>{$val->id}</td><td>{$val->name}</td><td>{$val->email}</td><td>{$val->password}</td>";
+          print "<td><a href='form.php?id={$val->id}' target='self' class='btn btn-outline-warning mx-2'><i class='fas fa-pencil-alt'></i></a>";
+          print "<a href='' target='self' class='btn btn-outline-danger mx-2'><i class='fas fa-trash'></i></a></td>";
+          print "</tr>";
+        }
+        print "</table>";
     }
-    print "</table>";
   }
 
   /**
--- form.php
+++ form.php
@@ -1,11 +1,12 @@
 <?php
 
 require('header.php');
+
 print "
 <div class='container'>
   <div class='row'>";
 print "<h5 class='text-muted my-2'>Usuários</h5>";
-$read = DefaultController::read('tb_user', '', '', '', '');
+$read = DefaultController::read('tb_user', "id = {$_GET['id']}", '', '', 'read');
 print "
   </div>
 </div>
